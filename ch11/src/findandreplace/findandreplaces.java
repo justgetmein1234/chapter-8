b3862b84e7a9f4b662ff31a3ea7efc31907ba165
justgetmein1234 <100795345+justgetmein1234@users.noreply.github.com>
2022-04-27 10:32:03 -0600

    Update findandreplaces.java

--- ch11/src/findandreplace/findandreplaces.java
+++ ch11/src/findandreplace/findandreplaces.java
@@ -1,11 +1,4 @@
-package findandreplace;
 
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
 public class findandreplaces {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
